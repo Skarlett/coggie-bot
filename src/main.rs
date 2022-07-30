53c0266be20b6dff3fd66766cf31b7503eba7858
Skarlett <360d@pm.me>
2022-07-30 02:49:55 -0500

    add bookmark retention feature

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,63 @@
 use std::env;
 
 use serenity::async_trait;
-use serenity::model::channel::{Message, ReactionType};
+use serenity::model::channel::{ReactionType};
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::Reaction;
+use serenity::model::Timestamp;
+use serenity::builder::CreateMessage;
 use serenity::prelude::*;
-
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn reaction_add(ctx: Context, ev: Reaction)
+    async fn reaction_add(&self, ctx: Context, ev: Reaction)
     {
         if let ReactionType::Unicode(x) = ev.emoji
         {
-            if !x.eq(String::from("\u{1F516}")) {
+            if !x.eq(&String::from("\u{1F516}")) {
                 return;
             }
 
             if let Some(user_id) = ev.user_id {
-                let msg = ev.channel_id.message(&ctx, ev.message_id);
+                let link: String = {
+                    if let Some(gid) = ev.guild_id {
+                        format!("{}/{}/{}", gid, ev.channel_id, ev.message_id)
+                    }
+                    else {
+                        String::from("N/A")
+                    }
+                };
 
-                let dm = user_id.to_user(&ctx)
-                       .expect("Couldn't find user")
-                       .create_dm_channel(&ctx)
-                       .send_message(|b|
-                            m.content("Hello, World!")
-                             .embed(|e| {
-                                 e.title("Bookmark")
-                                  .description("You bookmarked this message on {} from channel {} ({})")
-                                  .fields(vec![
-                                      ("link:", format!("", ev.message_id.link_ensured(&ctx)), true),
-                                      ("content:", msg.content, false),
-                                  ])
-                                  .footer(|f| f.text("https://github.com/Skarlett/coggie-bot"))
-                                  .timestamp(Timestamp::now())
-                             })
-                       )
-                       .await;
+                let msg = ev.channel_id
+                    .message(&ctx, ev.message_id).await.unwrap();
+
+                let attachments = msg.attachments
+                    .iter()
+                    .map(|c| format!("{}\n", c.url))
+                    .collect::<String>();
+
+                let _ = user_id.to_user(&ctx)
+                    .await
+                    .expect("Couldn't find user")
+                    .create_dm_channel(&ctx)
+                    .await
+                    .unwrap()
+                    .send_message(&ctx, |m: &mut CreateMessage| {
+                            m.embed(|e| {
+                                e.title("Bookmark")
+                                .description("You bookmarked this message on {} from channel {} ({})")
+                                .fields(vec![
+                                    ("link:", link, true),
+                                    ("attachments:", attachments, false),
+                                    ("content:", msg.content.to_string(), false),
+                                ])
+                                .footer(|f| f.text("https://github.com/Skarlett/coggie-bot"))
+                                .timestamp(Timestamp::now())
+                            })
+                        }
+                    )
+                    .await;
             }
         }
     }
@@ -56,9 +77,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+        Client::builder(&token, intents)
+            .event_handler(Handler)
+            .await
+            .expect("Err creating client");
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+    client.start().await?;
+    unreachable!();
 }
