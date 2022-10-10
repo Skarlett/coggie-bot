46e84605b8cb6041f6057b7f25c00a9572c0c7e3
skarlett <skarlett@github.com>
2022-10-10 01:26:44 -0500

    update `mkDerivation` to `naersk`

--- flake.nix
+++ flake.nix
@@ -2,57 +2,47 @@
   description = "Open source discord utility bot";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-    import-cargo.url = github:edolstra/import-cargo;
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, import-cargo }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux"  ];
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
       version = "${builtins.substring 0 8 lastModifiedDate}-${self.shortRev or "dirty"}";
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-    in {
-      overlay = final: prev: {
-        coggiebot = with final; final.callPackage ({ inShell ? false }: stdenv.mkDerivation rec {
-          name = "coggiebot-${version}";
-          src = if inShell then null else ./.;
-          buildInputs =
-            [ rustc
-              cargo
-            ] ++ (if inShell then [
-              # In 'nix develop', provide some developer tools.
-              rustfmt
-              clippy
-            ] else [
-              (import-cargo.builders.importCargo {
-                lockFile = ./Cargo.lock;
-                inherit pkgs;
-              }).cargoHome
-            ]);
 
-          target = "--release";
-          buildPhase = "cargo build ${target}  --frozen --offline";
-          doCheck = true;
-          checkPhase = "cargo test ${target} --frozen --offline";
-          installPhase =
-            ''
-              mkdir -p $out
-              cargo install --frozen --offline --path . --root $out
-              rm $out/.crates.toml
-            '';
-        }) {};
+    in rec {
+      inherit(flake-utils.lib.eachDefaultSystem(system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          naerk-lib = pkgs.callPackage naersk {};
+        in rec {
+          packages.coggiebot = naerk-lib.buildPackage {
+            src = ./.;
+          };
+          packages.default = packages.coggiebot;
+
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        }
+      )) packages devShell;
+
+      overlays.default = final: prev: {
+        coggiebot =
+          with final;
+          let pkgs = import <nixpkgs> { inherit system; };
+          in final.callPackage ({ inShell ? false }: packages {});
       };
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) coggiebot;
-        });
-
-      hydraJobs = forAllSystems (system: self.packages.${system}.coggiebot);
-      defaultPackage = forAllSystems (system: self.packages.${system}.coggiebot);
-      devShell = forAllSystems (system: self.packages.${system}.coggiebot.override { inShell = true; });
+      # hydraJobs = forAllSystems (system: self.packages.${system}.coggiebot);
+      # packages.default = forAllSystems (system: self.packages.${system}.coggiebot);
+      # devShell = forAllSystems (system: self.packages.${system}.coggiebot.override { inShell = true; });
 
       nixosModules.coggiebot =
         { pkgs, lib, config, coggiebot, ... }:
@@ -68,7 +58,6 @@
           };
 
           config = mkIf cfg.enable {
-            nixpkgs.overlays = [ self.overlay ];
             systemd.services.coggiebot = {
               wantedBy = [ "multi-user.target" ];
               after = [ "network.target" ];
