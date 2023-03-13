dc9dc60926943efef8e6129dad617bf25a48aebb
Skarlett <Skarlett@github.com>
2023-03-13 14:36:47 -0500

    clean up features

--- src/controllers/mockingbird/mod.rs
+++ src/controllers/mockingbird/mod.rs
@@ -3,4 +3,7 @@ pub mod mockingbird;
 #[cfg(feature = "demix")]
 pub mod demix;
 
+static FEATURE: &'static str = "mockingbird";
+
+
 pub use mockingbird::*;
--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -23,27 +23,30 @@ use serenity::async_trait;
 use serenity::model::{channel::Reaction, gateway::Ready};
 use serenity::prelude::*;
 
+macro_rules! add_commands {
+    ($framework:expr,
+        { $( [ $($feature:literal),* ] => [ $($group:expr),* ]),*}
+    )=>
+    {
+        $(#[cfg(all( $(feature = $feature),* ))]
+        { $framework = $framework$(.group(&$group))*; })*
+    }
+}
+
 #[allow(unused_mut)]
-pub fn setup_framework(mut cfg: StandardFramework) -> StandardFramework {
-    #[cfg(feature = "mockingbird")]
-    {
-        cfg = cfg.group(&mockingbird::MOCKINGBIRD_GROUP);
-    }
-
-    #[cfg(all(feature="demix", feature="mockingbird"))]
-    {
-        cfg = cfg.group(&mockingbird::DEMIX_GROUP);
-    }
-
-    #[cfg(feature = "basic-cmds")]
-    { cfg = cfg.group(&basic::COMMANDS_GROUP); }
-
-    #[cfg(feature = "list-feature-cmd")]
-    { cfg = cfg.group(&features::FEATURES_GROUP); }
-
-    #[cfg(feature = "prerelease")]
-    { cfg = cfg.group(&features::PRERELEASE_GROUP); }
-
+pub fn setup_framework(mut cfg: StandardFramework, client: ClientBuilder) -> StandardFramework {
+    add_commands!(
+        cfg,
+        {
+            ["mockingbird"] => [mockingbird::MOCKINGBIRD_GROUP],
+            ["basic-cmds"] => [basic::COMMANDS_GROUP],
+            ["prerelease"] => [features::PRERELEASE_GROUP::PRERELEASE_GROUP],
+            ["demix"] => [mockingbird::DEMIX_GROUP],
+            ["bookmark-emoji"] => [bookmark::BOOKMARK_GROUP],
+            ["list-feature-cmds"] => [features::LIST_FEATURE_CMDS_GROUP],
+            ["help-cmd"] => [features::HELP_GROUP]
+        }
+    );
     cfg
 }
 
