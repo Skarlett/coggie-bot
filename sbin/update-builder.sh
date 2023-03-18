d1b59d8dc551fe10142c2a6c037424ebd62292b6
Skarlett <Skarlett@github.com>
2023-03-18 12:43:45 -0500

    include possible fix for auto-updater

--- sbin/update-builder.sh
+++ sbin/update-builder.sh
@@ -55,7 +55,7 @@ if [[ "\$CHASH" != "\$LHASH" ]]; then
   /bin/systemctl daemon-reload
 
   systemctl restart ${coggiebotd}
-  systemctl restart ${coggiebotd-update-timer}
+  systemctl start ${coggiebotd-update-timer}
   echo "migrating finished"
 fi
 
