9128082d0284e2729d880f21cb6e4d47af691e18
Skarlett <Skarlett@github.com>
2023-04-23 05:19:31 -0500

    remove mistakes

--- sbin/stream-demix/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, buildPythonApplication, ... }:
-buildPythonApplication {
-  pname = "stream";
-  version = "0.1.0";
-  src = ./.;
-  propagatedBuildInputs = [ pkgs.python39Packages.deemix ];
-  doCheck = false;
-}
