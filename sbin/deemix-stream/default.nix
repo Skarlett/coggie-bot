6008b00c3bd573bc67ebc4c864b4dbbb62d3c704
Skarlett <Skarlett@github.com>
2023-05-07 14:48:33 -0500

    rm sbin/deemix-stream

--- sbin/deemix-stream/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, python39Packages, buildPythonApplication }:
-buildPythonApplication {
-  pname = "deemix-stream";
-  version = "0.1.0";
-  src = ./.;
-  propagatedBuildInputs = with python39Packages;
-    [ deemix ];
-  doCheck = false;
-}
