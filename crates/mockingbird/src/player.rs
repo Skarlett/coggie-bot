3442fc9b44cc9650a27fcdd2eb6bfe2d8dc29262
Skarlett <Skarlett@github.com>
2023-09-04 06:23:23 -0500

    replace evloop with delayed oneshots

--- crates/mockingbird/src/player.rs
+++ crates/mockingbird/src/player.rs
@@ -18,7 +18,7 @@ use songbird::{
     Songbird,
     Call, 
     create_player,
-    input::{Input, error::Error as SongbirdError}
+    input::{Input, error::Error as SongbirdError}, tracks::{Track, TrackHandle}
 };
 
 use std::{
@@ -45,6 +45,13 @@ struct BetterPlayer;
 // const MKBIRD_QUEUE_SCHEDULER_MS: &'static str = env!("MKBIRD_QUEUE_MAX_DURATION", "350");
 
 const TS_ZERO: Duration = Duration::from_secs(0);
+
+async fn next_track(call: &mut Call, uri: &str) -> Result<TrackHandle, HandlerError> {
+    let player = Players::from_str(&uri)
+        .ok_or_else(|| HandlerError::NotImplemented)?;
+        
+    player.play(call, &uri).await.map_err(HandlerError::from)
+}
 
 #[allow(unused_variables)]
 #[derive(Debug)]
@@ -83,6 +90,8 @@ impl std::fmt::Display for HandlerError {
         }
     }
 }
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
 
 impl std::error::Error for HandlerError {}
 
@@ -176,16 +185,16 @@ impl Players {
         else { return None }
     }
 
-    async fn play(&self, handler: &mut Call, uri: &str) -> Result<(), HandlerError>
+    async fn play(&self, handler: &mut Call, uri: &str) -> Result<TrackHandle, HandlerError>
     {
         let input = match self {
             Self::Deemix => ph_deemix_player(uri).await,
             Self::Ytdl => ph_ytdl_player(uri).await
         }?;
 
-        let (track, _track_handle) = create_player(input);
+        let (track, track_handle) = create_player(input);
         handler.enqueue(track);
-        Ok(())
+        Ok(track_handle)
     }
 
     async fn fan_collection(&self, uri: &str) -> Result<Vec<String>, HandlerError> {
@@ -198,14 +207,14 @@ impl Players {
     }
 }
 
-type LazyQueue = HashMap<GuildId, Arc<RwLock<VecDeque<String>>>>;
+type LazyQueue = HashMap<GuildId, Arc<QueueContext>>;
 pub struct LazyQueueKey;
 impl TypeMapKey for LazyQueueKey {
     type Value = LazyQueue;
 }
 
 
-struct QueueContext {
+pub struct QueueContext {
     guild_id: GuildId,
     invited_from: ChannelId,
     voice_chan_id: GuildChannel,
@@ -214,7 +223,6 @@ struct QueueContext {
     data: Arc<RwLock<TypeMap>>,
     manager: Arc<Songbird>,
     cold_queue: Arc<RwLock<VecDeque<String>>>,
-    lock: Mutex<()>
 }
 
 struct AbandonedChannel(Arc<QueueContext>);
@@ -237,103 +245,46 @@ impl VoiceEventHandler for AbandonedChannel {
     }
 }
 
-struct TrackEndNotifier(Arc<QueueContext>);
+
+struct Preload(Arc<QueueContext>);
 #[async_trait]
-impl VoiceEventHandler for TrackEndNotifier {
-    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
-        if let EventContext::Track(track_list) = ctx {
-            let cold_queue_len = self.0.cold_queue.read().await.len();
-            let hot_queue_len = track_list.len();
-            let total = cold_queue_len + hot_queue_len;
-
-            self.0
-                .invited_from
-                .say(&self.0.http, &format!("Tracks ended: ({} left).", total))
-                .await
-                .unwrap();
-            
-            tracing::info!("Hot queue: {}", track_list.len());
-            tracing::info!("Cold queue: {}", self.0.cold_queue.read().await.len());
-        }
-        None
-    }
-}
-
-struct GlobalPreloader(Arc<QueueContext>);
-
-impl GlobalPreloader {
-    async fn next_song(uri: &str, call: &mut Call) -> Result<(), HandlerError> {
-        let player = Players::from_str(&uri)
-            .ok_or_else(|| HandlerError::NotImplemented)?;
-        
-        player.play(call, &uri).await?;
-        Ok(())
-    }
-
-    async fn handler_error(&self, why: HandlerError, uri: &str) {
-        self.0.invited_from
-            .say(&self.0.http, format!("Failed to play song. [{}] ({})", uri, why))
-            .await
-            .unwrap();
-    }
-}
-
-#[async_trait]
-impl VoiceEventHandler for GlobalPreloader {
-    async fn act(&self, _ctx: &EventContext<'_>) -> Option<Event> {
-        const TS_PRELOAD_OFFSET: Duration = Duration::from_secs(20);
-        let before_lock = std::time::Instant::now();
-        let _ = self.0.lock.lock().await;
-
-        if before_lock.elapsed() > Duration::from_millis(1000) {
-            tracing::info!("Skipping preloader tick, lock took too long");
-            return None;
-        }
-
+impl VoiceEventHandler for Preload {
+    async fn act(&self, _ctx: &EventContext<'_>) -> Option<Event> {        
         if self.0.cold_queue.read().await.is_empty() {
             return None;
         }
 
-        match self.0.manager.get(self.0.guild_id) {
-            Some(call_lock) => {
-                let mut call = call_lock.lock().await;
-                let hotqueue = call.queue();
-                tracing::info!("Hot queue: {}/{}", hotqueue.len(), self.0.cold_queue.read().await.len());
-                match hotqueue.len() {
-                    1 => {
-                            let current = hotqueue.current().unwrap();
-                            let current_meta = current.metadata().clone();
-                            let state = current.get_info().await.unwrap();
-
-                            tracing::info!("Current: {:?}", current.metadata().duration);
-                            tracing::info!("State::position: {:?}", state.position);
-                            tracing::info!("State::playtime: {:?}", state.play_time);
-
-                            let total_duration = current_meta.duration.unwrap();
-                            
-                            if state.position >= total_duration - TS_PRELOAD_OFFSET {
-                                let next_song = self.0.cold_queue.write().await.pop_front().unwrap();
-                                let player = Players::from_str(&next_song).unwrap();
-                                player.play(&mut call, &next_song).await.unwrap();
-
-                                if let Err(why) = Self::next_song(&next_song, &mut call).await {
-                                    self.handler_error(why, &next_song).await;
-                                }
-                            }
-                        }
-
-                    0 => { 
-                        let next_song = self.0.cold_queue.write().await.pop_front().unwrap();
-                        if let Err(why) = Self::next_song(&next_song, &mut call).await {
-                            self.handler_error(why, &next_song).await;
-                        } 
-                    },
-                    _ => {}
-                }
-                return None
-            },
-            None => unreachable!()
+        if let Some(call_lock) = self.0.manager.get(self.0.guild_id) {
+            let mut call = call_lock.lock().await;
+            let uri = self.0.cold_queue.write().await.pop_front().unwrap();
+            
+            let handler = next_track(&mut call, &uri).await.unwrap();
+            handler.add_event(
+                Event::Delayed(handler.metadata().duration.unwrap() - Duration::from_secs(20)),
+                Preload(self.0.clone())
+            ).unwrap();
         }
+        None
+    }
+}
+struct TrackEndNotifier(Arc<QueueContext>);
+#[async_trait]
+impl VoiceEventHandler for TrackEndNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        tracing::info!("Cold queue: {}", self.0.cold_queue.read().await.len());
+        
+        if let EventContext::Track(track_list) = ctx {
+            let cold_queue_len = self.0.cold_queue.read().await.len();
+            let hot_queue_len = track_list.len();
+            tracing::info!("Hot queue: {}", hot_queue_len);
+            self.0
+                .invited_from
+                .say(&self.0.http, &format!("Tracks ended: ({} left).", hot_queue_len-1 + cold_queue_len))
+                .await
+                .unwrap();
+            
+        }
+        None
     }
 }
 
@@ -362,7 +313,7 @@ async fn leave_routine (
     Ok(())
 }
 
-async fn join_routine(ctx: &Context, msg: &Message) -> Result<ChannelId, JoinError> {
+async fn join_routine(ctx: &Context, msg: &Message) -> Result<Arc<QueueContext>, JoinError> {
     let guild = msg.guild(&ctx.cache).unwrap();
     let guild_id = guild.id;
 
@@ -387,28 +338,17 @@ async fn join_routine(ctx: &Context, msg: &Message) -> Result<ChannelId, JoinErr
 
     let (_handle_lock, success) = manager.join(guild_id, connect_to).await;
 
-
-
-
     if let Err(e) = success {
         return Err(e);
     }
 
-
-
     // cold queue 
-    let rc = Arc::new(RwLock::new(VecDeque::new()));
-    {
-        let mut glob = ctx.data.write().await; 
-        let queue = glob.get_mut::<LazyQueueKey>().expect("Expected LazyQueueKey in TypeMap");
-        queue.insert(guild_id, rc.clone());
-    }
     
     let call_lock = manager.get(guild_id).unwrap(); 
     let mut call = call_lock.lock().await;
-
+    
     let chan: Channel  = connect_to.to_channel(&ctx.http).await.unwrap();
-
+    
     let queuectx =
         if let Channel::Guild(voice_chan_id) = chan {
             QueueContext {
@@ -419,8 +359,7 @@ async fn join_routine(ctx: &Context, msg: &Message) -> Result<ChannelId, JoinErr
                 cache: ctx.cache.clone(),
                 data: ctx.data.clone(),
                 manager: manager.clone(),
-                cold_queue: rc.clone(),
-                lock: Mutex::new(())
+                cold_queue: Arc::new(RwLock::new(VecDeque::new())),
             }
         } else {
             tracing::error!("Expected voice channel (GuildChannel), got {:?}", chan);
@@ -435,17 +374,11 @@ async fn join_routine(ctx: &Context, msg: &Message) -> Result<ChannelId, JoinErr
     );
 
     call.add_global_event(
-        Event::Periodic(Duration::from_secs(5), Some(Duration::from_secs(1))),
-        GlobalPreloader(queuectx.clone())
-    );
-
-
-    call.add_global_event(
         Event::Periodic(Duration::from_secs(720), None),
         AbandonedChannel(queuectx.clone())
     );
 
-    Ok(connect_to)
+    Ok(queuectx)
 }
 
 #[command]
@@ -453,16 +386,16 @@ async fn join_routine(ctx: &Context, msg: &Message) -> Result<ChannelId, JoinErr
 #[only_in(guilds)]
 async fn now_playing(ctx: &Context, msg: &Message) -> CommandResult {
     let connect_to = join_routine(&ctx, msg).await;
-    
-
     if let Err(ref e) = connect_to {
         msg.channel_id
            .say(&ctx.http, format!("Failed to join voice channel: {:?}", e))
            .await?;        
     }
 
+    let connect_to = connect_to.unwrap();
+
     msg.channel_id
-       .say(&ctx.http, format!("Joined {}", connect_to.unwrap().mention()))
+       .say(&ctx.http, format!("{}: <link>", connect_to.voice_chan_id.mention()))
        .await?;
 
     Ok(())
@@ -481,7 +414,7 @@ async fn njoin(ctx: &Context, msg: &Message) -> CommandResult {
     }
 
     msg.channel_id
-       .say(&ctx.http, format!("Joined {}", connect_to.unwrap().mention()))
+       .say(&ctx.http, format!("Joined {}", connect_to.unwrap().voice_chan_id.mention()))
        .await?;
 
     Ok(())
@@ -559,27 +492,38 @@ async fn nqueue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         .expect("Songbird Voice client placed in at initialisation.")
         .clone();
 
-    match manager.get(guild_id) {
-        Some(_call_lock) => { },
+    let qctx: Arc<QueueContext>;
+    let mut play_song_immediate = false;
+    let mut range = 0..;
+
+    let call = match manager.get(guild_id) {
+        Some(call_lock) => {
+            qctx = ctx.data.write().await.get_mut::<LazyQueueKey>().unwrap().get_mut(&guild_id).unwrap().clone();
+            call_lock
+        },
         None => {
-            let connected_to = join_routine(ctx, msg).await;            
-            
-            if let Err(ref e) = connected_to {
+            let tmp = join_routine(ctx, msg).await;            
+            play_song_immediate = true;
+            range = 1..;
+
+            if let Err(ref e) = tmp {
                 msg.channel_id
                    .say(&ctx.http, format!("Failed to join voice channel: {:?}", e))
                    .await
                    .unwrap();        
                 return Ok(());
             };
-
+            qctx = tmp.unwrap();
             msg.channel_id
-                   .say(&ctx.http, format!("Joined voice channel: {:?}", connected_to.unwrap().mention()))
+                   .say(&ctx.http, format!("Joined voice channel: {:?}", qctx.voice_chan_id.mention()))
                    .await
                    .unwrap();
 
-            manager.get(guild_id).unwrap();
+            let call = manager.get(guild_id).ok_or_else(|| JoinError::NoCall);
+            call?
         }
     };
+
 
     match Players::from_str(&url)
         .ok_or_else(|| String::from("Failed to select extractor for URL"))
@@ -587,18 +531,21 @@ async fn nqueue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         Ok(player) => {
             let mut uris = player.fan_collection(url.as_str()).await?;
             let added = uris.len();
+            qctx.cold_queue.write().await.extend(uris.drain(range));    
 
-            {
-                let mut glob = ctx.data.write().await; 
-                let queue = glob.get_mut::<LazyQueueKey>().expect("Expected LazyQueueKey in TypeMap");
-                queue.get_mut(&guild_id).unwrap().write().await.extend(uris.drain(..));
-            }
-            
-            msg.channel_id
+            let mut call = call.lock().await;
+
+            if play_song_immediate || call.queue().len() == 0 {}
+
+            let handler = player.play(&mut call, &uris[0]).await?;
+            handler.add_event(
+               Event::Delayed(handler.metadata().duration.unwrap() - Duration::from_secs(20)),
+               Preload(qctx.clone())
+            ).unwrap();
+
+            msg.channel_id            
                .say(&ctx.http, format!("Added {} Song(s)", added))
-               .await?;
-            // let mut call = call.lock().await;
-            // player.play(&ctx.http, &mut call, &url).await?;
+               .await?;            
         },
 
         Err(_) => {
