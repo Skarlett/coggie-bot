3dfd67c264a10537699bc0d941e1ce3c343a3d11
Skarlett <Skarlett@github.com>
2023-09-19 03:47:28 -0500

    make cobblestone tools

--- sbin/spotrec/recommend.py
+++ sbin/spotrec/recommend.py
@@ -1,14 +1,18 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import deemix
+import click
+import json
+from deemix import generateDownloadObject, parseLink
 
 SpotifyClientCredentials = spotipy.oauth2.SpotifyClientCredentials
+CacheFileHandler = spotipy.cache_handler.CacheFileHandler
 
-def checkCredentials(cachedir):
-    cache_handler = CacheFileHandler(cachedir / ".auth-cache")
+def checkCredentials(id, secret):
+    cache_handler = CacheFileHandler(".auth-cache")
     
     client_credentials_manager = SpotifyClientCredentials(
-        client_id=self.credentials['clientId'],
-        client_secret=self.credentials['clientSecret'],
+        client_id=id,
+        client_secret=secret,
         cache_handler=cache_handler
     )
     
@@ -17,8 +21,23 @@ def checkCredentials(cachedir):
     
     return spotify
 
+@click.command()
+@click.option('-s', '--spt-id', type=str, help='Path to the config folder')
+@click.option('-ss', '--spt-secret', type=str, help='Path to the config folder')
+@click.option('-sc', '--spt-cache', type=str, help='Path to the config folder')
+@click.option('-l', '--limit', type=str, default="5", help='Path to the config folder')
+@click.argument('isrcs', nargs=-1, required=True)
+def main(spt_id, spt_secret, spt_cache, limit, isrcs):
 
-def getRecommendations(spotify, seeds={}, limit=10):
-    recommendations = spotify.recommendations(limit=limit, **seeds)
+    sp = checkCredentials(spt_id, spt_secret)
+    tracks =  [ sp.search("isrc:"+x) for x in isrcs ]; 
     
-    return recommendations
+    rec = sp.recommendations(seed_tracks=[
+        x['tracks']['items'][0]["uri"] for x in tracks
+    ], limit=limit)
+    
+    for x in rec["tracks"]:
+        print(x["external_urls"]["spotify"])
+
+if __name__ == '__main__':
+    main(auto_envvar_prefix='DEEMIX')
