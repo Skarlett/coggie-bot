68d386eb06399fc6bc7b5d7430901058ab197a66
allow <Skarlett@github.com>
2024-01-04 06:20:27 -0600

    allow raw HTTP requests to be streamed

--- sbin/deemix-stream/default.nix
+++ sbin/deemix-stream/default.nix
@@ -1,8 +1,8 @@
-{ lib, buildPythonApplication, deemix }:
+{ lib, buildPythonApplication, deemix, spotipy }:
 buildPythonApplication {
   pname = "deemix-stream";
   version = "0.0.4";
 
-  propagatedBuildInputs = [ deemix ];
+  propagatedBuildInputs = [ deemix spotipy ];
   src = lib.cleanSource ./.;
 }
